20c2ee2a1b4f78d48d96fce391a8fcc5c925eb0e
Randi Stella <RANDINICOLESTELLA@GMAIL.COM>
2023-04-13 13:05:56 -0500

    error and success msg

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,16 @@
 const inputBtn = document.getElementById('input')
 const submitBtn = document.getElementById('button')
+const errorMsg = document.getElementById('error')
+const successMsg = document.getElementById('success')
 
-submitBtn.addEventListener('click', function() {
-    if (inputBtn && inputBtn.value) {
-        alert('my input has a value')
-    } else {
-        inputBtn.value === ''
+submitBtn.addEventListener('click', function () {
+       if (inputBtn.value === '') {
         inputBtn.style.border = '2px solid red'
-        inputBtn.innerHTML = 'error'
+        errorMsg.textContent = "Please enter a valid email"
+        successMsg.textContent = ''
+       } else {
+        inputBtn.style.border = '2px solid green'
+        errorMsg.textContent = ''
+        successMsg.textContent = 'Thank you!'
     }
 })
